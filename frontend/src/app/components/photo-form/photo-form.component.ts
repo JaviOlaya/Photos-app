41fd5ea65d897451e55eab7255d93a4c4dbce899
javiOlaya <jjosaez@gmail.com>
2019-10-17 00:46:28 +0200

    finishing the frontend

--- frontend/src/app/components/photo-form/photo-form.component.ts
+++ frontend/src/app/components/photo-form/photo-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PhotoService}from '../../services/photo-service.service';
+interface HtmlInputEvent extends Event{
+  target: HTMLInputElement & EventTarget;
+}
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+  file: File; 
+  photoSelected: string | ArrayBuffer ;
+  constructor(private  photoService: PhotoService) { }
 
   ngOnInit() {
   }
+  onPhotoSelected(event: HtmlInputEvent): void{
+    if(event.target.files && event.target.files[0]){
+      this.file = <File>event.target.files[0];
+      //
+      const file_reader = new FileReader();
+      file_reader.onload = e => this.photoSelected = file_reader.result;
+      file_reader.readAsDataURL(this.file);
+    }
+  }
 
+  uploadPhoto(title: HTMLInputElement, description: HTMLTextAreaElement): boolean {
+    this.photoService.createPhoto(title.value, description.value, this.file)
+    .subscribe(res => console.log(res), err => console.log(err))
+    return false; // se introduce para evitar el reinicio 
+  }
 }
--- frontend/src/app/services/photo-service.service.ts
+++ frontend/src/app/services/photo-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class PhotoServiceService {
+export class PhotoService {
 
-  constructor() { }
+  URI = 'http://localhost:4000/api/photos'
+
+  constructor(private http: HttpClient) { }
+
+  createPhoto(title: string, description: string, photo: File){
+   const fData= new FormData();
+   fData.append('title',title);
+   fData.append('description',description);
+   fData.append('image',photo);
+    return this.http.post(this.URI,fData);
+  }
+
 }
